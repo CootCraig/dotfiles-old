23a4317d2346910d8a7cabd6bba63ff0aad1bf19
Craig H Anderson <craig@coot.net>
2011-03-07 15:06:20 -0700

    Add ~/.zshenv and ~/.zshrc

--- symlink.sh
+++ symlink.sh
@@ -1,2 +1,4 @@
 ln --symbolic --interactive vim ~/.vim
 ln --symbolic --interactive vimrc ~/.vimrc
+ln --symbolic --interactive zshenv ~/.zshenv
+ln --symbolic --interactive zshrc ~/.zshrc
---
+++ zshenv
@@ -0,0 +1,2 @@
+export RUBYMINE_JDK='/usr/lib/jvm/java-6-sun'
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"  # This loads RVM into a shell session.
